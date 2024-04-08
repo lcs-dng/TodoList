adbcbab80fd4ba78e9dd242a4573993a6207507e
Dexter Ng <DNg@lcs.on.ca>
2024-04-08 13:46:24 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Dexter Ng on 2024-04-08.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
