8b562becfead00beb43ce971b6d01d8961bcec4e
Dexter Ng <DNg@lcs.on.ca>
2024-04-08 15:00:05 -0400

    Extracted a subview to handle drawing the to-do items.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -26,28 +26,19 @@ struct LandingView: View {
                 
                 List {
                     
-                    Label(
-                        title: {
-                            Text("Study for Chemistry quiz")
-                        }, icon: {
-                            Image(systemName: "circle")
-                        }
+                    ItemView(
+                        title: "Study for Chemistry quiz",
+                        done: false
                     )
                     
-                    Label(
-                        title: {
-                            Text("Finish Computer Science assignment")
-                        }, icon: {
-                            Image(systemName: "circle")
-                        }
+                    ItemView(
+                        title: "Finish Computer Science assignment",
+                        done: true
                     )
                     
-                    Label(
-                        title: {
-                            Text("Go for a run around campus")
-                        }, icon: {
-                            Image(systemName: "circle")
-                        }
+                    ItemView(
+                        title: "Go for a run around campus",
+                        done: false
                     )
 
                 }
@@ -72,4 +63,24 @@ struct LandingView: View {
 
 #Preview {
     LandingView()
+}
+
+struct ItemView: View {
+    
+    let title: String
+    let done: Bool
+    
+    var body: some View {
+        Label(
+            title: {
+                Text(title)
+            }, icon: {
+                if done == true {
+                    Image(systemName: "checkmark.circle")
+                } else {
+                    Image(systemName: "circle")
+                }
+            }
+        )
+    }
 }
