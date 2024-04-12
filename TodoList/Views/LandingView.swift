c24e0b4ab36968ca33b5a6263ff17de9910cc8f6
Dexter Ng <DNg@lcs.on.ca>
2024-04-12 09:14:15 -0400

    Can now mark item as being done or not by tapping.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -27,7 +27,7 @@ struct LandingView: View {
             
             VStack {
                 
-                List(todos) { todo in
+                List($todos) { $todo in
 
                     ItemView(currentItem: todo)
                         // Delete a to-do item
@@ -39,6 +39,10 @@ struct LandingView: View {
                                     delete(todo)
                                 }
                             )
+                        }
+                        // Tap to mark as done
+                        .onTapGesture {
+                            todo.done.toggle()
                         }
                     
                 }
